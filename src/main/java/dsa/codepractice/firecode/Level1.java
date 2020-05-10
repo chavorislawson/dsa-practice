9ef659293d3bb7080243f1208ff93838549f26ab
Clawson13 <chavoris.lawson.jobs@gmail.com>
2020-05-10 01:24:48 -0400

    added more firecode solutions, made some changes to java docs and started adding time and space complexities for solutions.

--- src/main/java/dsa/codepractice/firecode/Level1.java
+++ src/main/java/dsa/codepractice/firecode/Level1.java
@@ -2,16 +2,6 @@ package dsa.codepractice.firecode;
 
 public class Level1 {
 
-    /**
-     * returns fibonacci sequence up to n
-     * 
-     * @param n
-     * @return
-     */
-    public int fib(int n){
-        return n<2 ? n : fib(n-1)+fib(n-2);
-    }
-
     // public static void flipItVerticalAxis(int[][] matrix) {
     //     int[][] newM;
     //     for (int r = 0; r < matrix[r].length; r++) {
--- src/main/java/dsa/codepractice/firecode/Level2.java
+++ src/main/java/dsa/codepractice/firecode/Level2.java
@@ -1,5 +1,6 @@
 package dsa.codepractice.firecode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import dsa.datastructures.linkedList.Node;
@@ -7,14 +8,20 @@ import dsa.datastructures.linkedList.Node;
 public class Level2 {
 
     /**
-     * are they the same length?
-     * are they the same case?
-     * do they contain letters?
-     * are they empty?
-     * is each letter in one in two?
+     * returns fibonacci sequence up to n
      * 
-     * time complexity: O(n) linear
-     * space complexity: O(1) Constant
+     * @param n
+     * @return
+     */
+    public int fib(int n) {
+        return n < 2 ? n : fib(n - 1) + fib(n - 2);
+    }
+
+    /**
+     * are they the same length? are they the same case? do they contain letters?
+     * are they empty? is each letter in one in two?
+     * 
+     * time complexity: O(n) linear space complexity: O(1) Constant
      * 
      * @param one
      * @param two
@@ -25,7 +32,8 @@ public class Level2 {
         if (one.length() != two.length()) {
             return false;
         }
-        int[] letters = new int[256];// because there are 256 ASCII characters. Not common sense, but it's common knowledge.
+        int[] letters = new int[256];// because there are 256 ASCII characters. Not common sense, but it's common
+                                     // knowledge.
         char[] c = one.toCharArray();
 
         for (char a : c) {
@@ -47,20 +55,22 @@ public class Level2 {
      * @param root
      * @return
      */
-    public int findMax(TreeNode root){
-        int rtd = Integer.MIN_VALUE, ld = Integer.MIN_VALUE,rd = Integer.MIN_VALUE, max=Integer.MIN_VALUE;
-        ld = 0; rtd =0; rd=0;
-        if(root!=null){
+    public int findMax(TreeNode root) {
+        int rtd = Integer.MIN_VALUE, ld = Integer.MIN_VALUE, rd = Integer.MIN_VALUE, max = Integer.MIN_VALUE;
+        ld = 0;
+        rtd = 0;
+        rd = 0;
+        if (root != null) {
             rtd = root.data;
             ld = findMax(root.left);
             rd = findMax(root.right);
-            if(ld>rd){
-                max=ld;
-            }else{
-                max=rd;
+            if (ld > rd) {
+                max = ld;
+            } else {
+                max = rd;
             }
-            if(rtd>max){
-                max=rtd;
+            if (rtd > max) {
+                max = rtd;
             }
         }
         return max;
@@ -72,11 +82,12 @@ public class Level2 {
      * 
      * <p>
      * O(n) Time, O(log n) space
+     * 
      * @param root
      * @return
      */
-    public int size(TreeNode root){
-        return root == null ? 0 : size(root.right)+1+size(root.left);
+    public int size(TreeNode root) {
+        return root == null ? 0 : size(root.right) + 1 + size(root.left);
     }
 
     /**
@@ -88,8 +99,8 @@ public class Level2 {
      * @param root
      * @return
      */
-    public boolean validateBST(TreeNode root){
-        return validateBST(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
+    public boolean validateBST(TreeNode root) {
+        return validateBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 
     /**
@@ -100,18 +111,21 @@ public class Level2 {
      * @param max
      * @return
      */
-    public boolean validateBST(TreeNode root, int min, int max){
-        if(root==null){
+    public boolean validateBST(TreeNode root, int min, int max) {
+        if (root == null) {
             return true;
-        }if(root.data<=min||root.data>=max){
+        }
+        if (root.data <= min || root.data >= max) {
             return false;
         }
-        return validateBST(root.left,min,root.data)&&validateBST(root.right, root.data, max);
+        return validateBST(root.left, min, root.data) && validateBST(root.right, root.data, max);
     }
 
     /**
      * <p>
-     * Checks if two strings are isomorphic of each other. This needs further explanation.
+     * Checks if two strings are isomorphic of each other. This needs further
+     * explanation. Ex. dss and cll are isomorphic because they share the same
+     * structure
      * 
      * <p>
      * O(n) space and time;
@@ -120,23 +134,23 @@ public class Level2 {
      * @param two
      * @return
      */
-    public boolean isIsomorphic(String one, String two){
-        if(one.length()<two.length()||two.isEmpty()){
+    public boolean isIsomorphic(String one, String two) {
+        if (one.length() < two.length() || two.isEmpty()) {
             return true;
         }
         HashMap<Character, Integer> o = new HashMap<>();
         HashMap<Character, Integer> t = new HashMap<>();
 
-        for(int i=0;i<two.length();i++){
+        for (int i = 0; i < two.length(); i++) {
             char c1 = one.charAt(i);
-            int val1 = o.get(c1)==null ? 1 : o.get(c1)+1;
-            o.put(c1,val1);
+            int val1 = o.get(c1) == null ? 1 : o.get(c1) + 1;
+            o.put(c1, val1);
 
             char c2 = two.charAt(i);
-            int val2 = t.get(c2)==null ? 1 : t.get(c2)+1;
-            t.put(c2,val2);
+            int val2 = t.get(c2) == null ? 1 : t.get(c2) + 1;
+            t.put(c2, val2);
 
-            if(!o.get(c1).equals(t.get(c2))){
+            if (!o.get(c1).equals(t.get(c2))) {
                 return false;
             }
 
@@ -145,39 +159,80 @@ public class Level2 {
     }
 
     /**
-	 * removes node at specified index +
-	 * 
-	 * @param index
-	 * @return
-	 */
-	public Node remove(Node head,int index){
-		if(index==1){
-			return head==null ? head : head.getNext(); 
-		}
-		Node curr = head;
-		Node prev = head;
+     * removes node at specified index +
+     * 
+     * @param index
+     * @return
+     */
+    public Node remove(Node head, int index) {
+        if (index == 1) {
+            return head == null ? head : head.getNext();
+        }
+        Node curr = head;
+        Node prev = head;
 
-		while(curr.getNext()!=null){
-			--index;
-			if(index==0){
-				prev.setNext(curr.getNext());
-				curr = null;
-			}
-			prev=curr;
-			curr=curr.getNext();
-		}
-		return head;
-	}
-
-    public class TreeNode{
-    int data;
-    TreeNode left;
-    TreeNode right;
-    
-    public TreeNode(int data, TreeNode left, TreeNode right){
-        this.data = data;
-        this.left = left;
-        this.right = right;
+        while (curr != null) {
+            --index;
+            if (index == 0) {
+                prev.setNext(curr.getNext());
+                curr.setNext(null);
+            }
+            prev = curr;
+            curr = curr.getNext();
+        }
+        return head;
     }
+
+    /**
+     * recursive pre-order traversal of Binary Tree
+     * 
+     * <p>
+     * This shit was easy asf, and I did not expect it to be. Got it basically on my
+     * first attempt. just missed some closing marks
+     * 
+     * <p>
+     * O(n) Time and O(log n) Space - recursion overhead in average case
+     * 
+     * @param list
+     * @param root
+     */
+    public void preorder(ArrayList<Integer> list, TreeNode root) {
+        if (root != null) {
+            list.add(root.data);
+            preorder(list, root.left);
+            preorder(list, root.right);
+        }
+    }
+
+    /**
+     * recursively adds "*" between successive characters. ex. abba = ab*ba
+     * 
+     * <p>
+     * o(n) Time
+     * 
+     * @param s
+     * @return
+     */
+    public String insertStarPair(String s) {
+        if (s == null || s.length() == 1) {
+            return s;
+        } else if (s.substring(0, 1).equals(s.substring(1, 2))) {
+            return s.substring(0, 1) + "*" + insertStarPair(s.substring(1, s.length()));
+        } else {
+            return s.substring(0, 1) + insertStarPair(s.substring(1, s.length()));
+        }
+
+    }
+
+    public class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int data, TreeNode left, TreeNode right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
     }
 }
