2543566b1c2e976d0db2a792e276d4e57282b316
Chavoris Lawson <clawson13@student.clayton.edu>
2020-05-23 18:43:26 -0400

    restarting App

--- src/main/java/dsa/app/App.java
+++ src/main/java/dsa/app/App.java
@@ -10,8 +10,6 @@ import dsa.codepractice.firecode.Level2;
  */
 public class App {
     public static void main(String[] args) {
-        Level2 l2 = new Level2();
-        String a = l2.compressText("abcefgggggggghhhhhhhhhhhh");
-        System.out.println(a);
+        System.out.println("Hello World");
     }
 }
