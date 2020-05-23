b268a18e8074e3577bb65865e2ba87c41b6060ac
Chavoris Lawson <clawson13@student.clayton.edu>
2020-05-23 00:33:31 -0400

    implemented TreeNode innerclass in Tree class

--- src/main/java/dsa/datastructures/trees/Tree.java
+++ src/main/java/dsa/datastructures/trees/Tree.java
@@ -2,38 +2,50 @@ package dsa.datastructures.trees;
 
 import java.util.LinkedList;
 
-import dsa.codepractice.firecode.Level2.TreeNode;
-
 public class Tree {
 
-    
-    // public static String levelOrder(TreeNode root){
-    //     String s = "";
-    //    LinkedList<TreeNode> q = new LinkedList<>();
-    //    if(root==null){
-    //      return s;
-    //    }
-    //    q.add(root);
-    //    q.add(null);
-       
-    //    while(!q.isEmpty()){
-    //      TreeNode curr = q.poll();
-         
-    //      if(curr==null){
-    //        if(!q.isEmpty()){
-    //          q.add(null);
-             
-    //        }
-    //      }else{
-    //        if(curr.left!=null){
-    //          q.add(curr.left);
-    //        }
-    //        if(curr.right!=null){
-    //          q.add(curr.right);
-    //        }
-    //        s = s+ Integer.toString(curr.data)+",";
-    //      }
-    //    }
-    //    return s.substring(0,s.length()-1);
-    //  }
+    /**
+     * iterative level order (BFS) traversal of a tree
+     * 
+     * <p>
+     * O(n) Time and O(log n) Space
+     * 
+     * @param root
+     * @return
+     */
+    public static String levelOrder(TreeNode root) {
+        String s = "";
+        LinkedList<TreeNode> q = new LinkedList<>();
+        if (root == null) {
+            return s;
+        }
+        q.add(root);
+        q.add(null);
+
+        while (!q.isEmpty()) {
+            TreeNode curr = q.poll();
+
+            if (curr == null) {
+                if (!q.isEmpty()) {
+                    q.add(null);
+
+                }
+            } else {
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+                s = s + Integer.toString(curr.data) + ",";
+            }
+        }
+        return s.substring(0, s.length() - 1);
+    }
+
+    private class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+    }
 }
